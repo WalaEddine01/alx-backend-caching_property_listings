124ea748cdb79bc221d795a2d8827ff9f2da6791
WalaEddine01 <walaaeddine33@gmail.com>
2025-07-12 23:55:08 +0100

    Made changes on 23:55:08

--- properties/views.py
+++ properties/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 from django.http import JsonResponse
-from .models import Property
+from ..properties.utils import get_all_properties
 
 @cache_page(60 * 15)
 def property_list(request):
-    properties = Property.objects.all()
+    properties = get_all_properties()
     data = [
         {
             "id": prop.id,
